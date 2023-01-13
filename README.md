3ec6fbc829514be76cad99b1af4d2f51f798188f
mingming.tang <mingmingtang@aliyun.com>
2023-01-13 17:34:45 +0800

    update contract and readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Prototype
+# SPex is a decentralized storage provider exchange space on FVM
 
+### Website
+https://www.spex.website/
+
+### Prototype
 https://github.com/SPex-team/prototype
+
+### Frontend
+https://github.com/SPex-team/spex-front
--- contracts/spex.sol
+++ contracts/spex.sol
@@ -11,6 +11,7 @@ contract SPEX {
         address seller;
         uint price;
         uint listTime;
+        string email;
     }
 
     struct HistoryRecord {
@@ -21,33 +22,80 @@ contract SPEX {
         uint dealTime;
     }
 
-    mapping(string => address) contractMiners;
+    mapping(string => address) _contractMiners;
 
-    mapping(string => ListMiner) listMiners;
+    mapping(string => ListMiner) _listMiners;
 
-    mapping (address => uint) name;
+    mapping (address => uint) _name;
 
-    function confirmChangeOwnerToSpex(string calldata minerId, string calldata sign) public {
-        
+    string[] _listMinerIds;
+
+    address _feeTo;
+    address _manager;
+
+    int _feeRate;
+
+    constructor(address manager, address feeTo, int feeRate) {
+        _manager = manager;
+        _feeTo = feeTo;
+        _feeRate = feeRate;
     }
 
-    function listMiner(string calldata minerId, uint price) public {
-        // Miner memory miner = miners[minerId];
-        // require(miner.isOnSale == false, "Miner already in list");
-        // miner.isOnSale = true;
-        // miner.price = price;
+    function changeFeeTo(address newFeeTo) public {
+        require(msg.sender == _manager, "Must manager can change");
+        _feeTo = newFeeTo;
+    }
+
+    function changeFeeRate(int newFeeRate) public {
+        require(msg.sender == _manager, "Must manager can change");
+        _feeRate = newFeeRate;
+    }
+
+    function confirmChangeOwnerToSpex(string calldata minerId, string calldata sign) public {
+        // verify signature
+        // accept miner
+
+        _contractMiners[minerId] = msg.sender;
+    }
+
+    function listMiner(string calldata minerId, uint price, string calldata email) public {
+        require(_contractMiners[minerId]==msg.sender, "You are not the owner of miner");
+        require(_listMiners[minerId].listTime == 0, "Miner already list");
+        ListMiner memory miner = ListMiner ({
+            id: minerId,
+            seller: msg.sender,
+            price: price,
+            listTime: block.timestamp,
+            email: email
+        });
+        _listMiners[minerId] = miner;
+        _listMinerIds.push(minerId);
     }
 
     function changePrice(string calldata minerId, uint price) public {
-        // Miner memory miner = miners[minerId];
-        // require(miner.isOnSale == true, "Miner not list yet");
-        // miner.price = price;
+        require(_contractMiners[minerId]==msg.sender, "You are not the owner of miner");
+        ListMiner memory miner = _listMiners[minerId];
+        miner.price = price;
     }
 
-    function changeOwner(string calldata minderId, string calldata newOwner) public {
-
+    function changeOwner(string calldata minerId, string calldata newOwner) public {
+        require(_contractMiners[minerId]==msg.sender, "You are not the owner of miner");
+        require(_listMiners[minerId].listTime == 0, "You must cancel list first");
+        // set owner to new owner
     }
 
+    function cancelList(string calldata minerId) public {
+        require(_contractMiners[minerId]==msg.sender, "You are not the owner of miner");
+        require(_listMiners[minerId].listTime == 0, "Miner not list");
+        delete _listMiners[minerId];
+    }
+
+    // function buyMiner(string calldata minerId) public {
+    //     ListMiner calldata listMiner = _listMiners[minerId];
+    //     require(listMiner.listTime == 0, "Miner not list");
+    //     ListMiner.seller.transfer()
+    //     delete _listMiners[minerId];
+    // }
 
     function getListMiners() view public {
         
@@ -58,8 +106,8 @@ contract SPEX {
         
     }
 
-    function getAvailableBalance(bytes memory target) public returns (int256) {
-        MinerTypes.GetAvailableBalanceReturn memory r = MinerAPI.getAvailableBalance(target);
-        return 123;
-    }
+    // function getAvailableBalance(bytes memory target) public returns (int256) {
+    //     MinerTypes.GetAvailableBalanceReturn memory r = MinerAPI.getAvailableBalance(target);
+    //     return 123;
+    // }
 }
